069cb3e8449b42c3e043ded4b893ff3995017009
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-07-29 15:20:47 +0300

    Add cors packadge

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const auth = require('./routes/authRoutes');
 
 const app = express();
 
-mongoose.connect(require('./config/secret').url, {
-  useNewUrlParser: true
-}).then(()=>console.log('DB connected')).catch(err=>console.log(err));
+mongoose
+  .connect(require('./config/secret').url, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log(err));
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'DELETE', 'PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
